5a9dd63dafc55d0225254299cbeaaee1d3129e06
Kayla McDanel <83437129+sowdapop@users.noreply.github.com>
2023-01-29 21:17:33 -0500

    attributions

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,3 +1,10 @@
+// Title: nodebucket
+// Author: Professor Krasso
+// Date: 20 Jan 2023
+// Modified by: Kayla McDanel
+// Attributions: Code from Bellevue Web Dev GitHub Repository
+// https://github.com/buwebdev/web-450
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
